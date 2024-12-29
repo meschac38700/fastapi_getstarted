82f933bf293f1e9da8e40640be813eb7a09615f8
Eliam LOTONGA <e.m.lotonga@gmail.com>
2024-12-29 17:03:16 +0100

    Keep only one secret_key endpoint

--- src/main.py
+++ src/main.py
@@ -4,9 +4,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/", name="Generate secret key")
-@app.get("/{length}", name="Generate secret key of a specific length")
+@app.get("/", name="Generate secret key.")
 def secret_key(length: int = 65):
     secret = secrets.token_urlsafe(length)
     return {"secret": secret}
