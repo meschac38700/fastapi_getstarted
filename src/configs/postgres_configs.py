56ae5dacdce839d1a074f3617fb57489a7857b55
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-04 07:01:57 +0100

    :hammer: Fix communication with database(insert/select)

--- src/configs/postgres_configs.py
+++ src/configs/postgres_configs.py
@@ -3,26 +3,28 @@ from typing import Annotated
 
 from fastapi import Depends
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from settings.generic import DATABASE_ENV_FILE
 
 
 class DatabaseSettings(BaseSettings):
+    _env_file = DATABASE_ENV_FILE
+    _env_prefix = "db"
     postgres_user: str = "fastapi"
     postgres_password: str = "fastapi"
     postgres_db: str = "fastapi"
-    host: str = "localhost"
-    port: int = 5432
+    host_db: str = "localhost"
+    port_db: int = 5432
     model_config = SettingsConfigDict(env_file=DATABASE_ENV_FILE)
 
     def get_engine(self):
-        return create_engine(self.uri)
+        engine = create_async_engine(self.uri)
+        return engine
 
     @property
     def uri(self):
-        breakpoint()
-        return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.host}:{self.port}/{self.postgres_db}"
+        return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.host_db}:{self.port_db}/{self.postgres_db}"
 
 
 @lru_cache
