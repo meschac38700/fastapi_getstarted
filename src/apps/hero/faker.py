b95630f74dd8ac57b8e553ec9b0db3f8b273d781
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-05 13:34:20 +0100

    :technologist: Add service for database queries

--- src/apps/hero/faker.py
+++ src/apps/hero/faker.py
@@ -1,21 +1,15 @@
-from sqlmodel.ext.asyncio.session import AsyncSession
-
-from settings import db_settings
+from services import db_service
 
 from .models import Hero
 
 
 async def fake_heroes():
-    engine = db_settings.get_engine()
-    async with AsyncSession(engine) as session:
-        session.add_all(
-            [
-                Hero(name="Spider-Boy", secret_name="Pedro Parqueador"),
-                Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48),
-                Hero(name="Iron man", secret_name="Robert Downey Jr", age=59),
-                Hero(name="Captain America", secret_name="Chris Evans", age=43),
-                Hero(name="Superman", secret_name="Henry Cavill", age=41),
-                Hero(name="Deadpond", secret_name="Dive Wilson"),
-            ]
-        )
-        await session.commit()
+    heroes = [
+        Hero(name="Spider-Boy", secret_name="Pedro Parqueador"),
+        Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48),
+        Hero(name="Iron man", secret_name="Robert Downey Jr", age=59),
+        Hero(name="Captain America", secret_name="Chris Evans", age=43),
+        Hero(name="Superman", secret_name="Henry Cavill", age=41),
+        Hero(name="Deadpond", secret_name="Dive Wilson"),
+    ]
+    await db_service.insert_batch(heroes)
