83a845725393dff5f25e7f2e6c8b7a7d1b16280a
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-05 13:57:30 +0100

    :hammer: Add all function to db service

--- src/services/db.py
+++ src/services/db.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, select
 
 from settings import db_settings
 
@@ -12,10 +12,21 @@ class DBService:
     def __init__(self):
         self._engine = db_settings.get_engine()
 
-    async def insert_batch(self, items: list[SQLModel]):
+    async def insert_batch(self, instances: list[SQLModel]):
+        """Insert a batch of SQLModel instances."""
+
         async with AsyncSession(self._engine) as session:
-            session.add_all(items)
+            session.add_all(instances)
             await session.commit()
+
+    async def all(self, model: SQLModel, *, offset: int = 0, limit: int = 100):
+        """Get all items of the given model."""
+
+        async with AsyncSession(self._engine) as session:
+            # based on https://docs.sqlalchemy.org/en/14/orm/extensions/asyncio.html#dynamic-asyncio
+            heroes = await session.scalars(select(model).offset(offset).limit(limit))
+
+        return heroes.all()
 
     # TODO(Complete with others functions)
 
