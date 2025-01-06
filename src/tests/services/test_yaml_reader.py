6c52c4b9f2c82a23d3454bf149779700580b4338
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-06 12:03:58 +0100

    :white_check_mark: Add test service YAMLReader

---
+++ src/tests/services/test_yaml_reader.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from unittest import TestCase
+
+from services.yaml_reader import YAMLReader
+
+
+class TestYAMLReader(TestCase):
+    def setUp(self):
+        self.reader = YAMLReader(Path(__file__).parent / "data" / "heroes.yaml")
+
+    def test_yaml_to_dict(self):
+        expected = {
+            "heroes": [
+                {"name": "Captain America", "age": 43, "secret_name": "Chris Evans"},
+                {"name": "Superman", "age": 41, "secret_name": "Clark Kent"},
+                {"name": "Deadpool", "secret_name": "Ryan Reynolds"},
+            ]
+        }
+        actual = self.reader.to_dict()
+
+        self.assertDictEqual(expected, actual)
