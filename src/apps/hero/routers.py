0d3d071cd3886c4e3ed079e00f7697d2f067c86b
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-08 18:25:16 +0100

    :adhesive_bandage: Rename endpoint to an appropriate name

--- src/apps/hero/routers.py
+++ src/apps/hero/routers.py
@@ -11,6 +11,6 @@ routers = APIRouter(tags=["heroes"], prefix="/heroes")
 
 
 @routers.get("/", name="Get heroes")
-async def get_items(offset: int = 0, limit: Annotated[int, Query(le=100)] = 100):
+async def get_heroes(offset: int = 0, limit: Annotated[int, Query(le=100)] = 100):
     async with DBService() as db_service:
         return await db_service.all(Hero, offset=offset, limit=limit)
