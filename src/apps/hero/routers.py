9a2b8934ddd7a8042270f4ad6769110320d8c082
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-08 20:29:03 +0100

    :hammer: Added GET hero endpoint

--- src/apps/hero/routers.py
+++ src/apps/hero/routers.py
@@ -10,6 +10,11 @@ from .models import Hero
 routers = APIRouter(tags=["heroes"], prefix="/heroes")
 
 
+@routers.get("/{pk}", name="Get hero by id.")
+async def get_hero(pk: int, db_service: DBServiceDep):
+    return await db_service.get(Hero, Hero.id == pk)
+
+
 @routers.get("/", name="Get heroes")
 async def get_heroes(
     db_service: DBServiceDep,
