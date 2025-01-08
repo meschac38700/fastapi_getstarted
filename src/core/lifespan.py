c28f8f983ead6c7285fcc6a7ae11dacce26cdd18
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-08 01:09:13 +0100

    :hammer: Alembic will create table

--- src/core/lifespan.py
+++ src/core/lifespan.py
@@ -1,11 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncEngine
 
-from core.db import create_db_and_tables
-
 
 async def setup(engine: AsyncEngine):
     """Script to be run after fastapi setup."""
-    await create_db_and_tables(engine)
+    # await create_db_and_tables(engine)
 
 
 async def teardown(engine: AsyncEngine):
