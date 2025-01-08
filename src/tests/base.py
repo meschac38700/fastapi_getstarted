9313922b1c5d85c491efeef6ebe4838ce0db16a4
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-08 10:23:18 +0100

    :hammer: Init db_service in parent test class

--- src/tests/base.py
+++ src/tests/base.py
@@ -1,6 +1,7 @@
 from unittest.async_case import IsolatedAsyncioTestCase
 
 from core.db import create_db_and_tables, delete_db_and_tables
+from services.db import DBService
 from settings import settings
 
 BASE_URL = "http://test"
@@ -11,7 +12,9 @@ class AsyncTestCase(IsolatedAsyncioTestCase):
     async def asyncSetUp(self):
         await super().asyncSetUp()
         await create_db_and_tables(_engine)
+        self.db_service = DBService()
 
     async def asyncTearDown(self):
         await super().asyncTearDown()
         await delete_db_and_tables(_engine)
+        await self.db_service.dispose()
--- src/tests/services/test_db_queries.py
+++ src/tests/services/test_db_queries.py
@@ -2,7 +2,6 @@ import pytest
 
 from apps.hero import Hero
 from apps.hero.faker import fake_heroes, hero_list
-from services.db import DBService
 from tests.base import AsyncTestCase
 
 BASE_URL = "http://test"
@@ -10,14 +9,6 @@ BASE_URL = "http://test"
 
 @pytest.mark.anyio
 class TestDBService(AsyncTestCase):
-    async def asyncSetUp(self):
-        await super().asyncSetUp()
-        self.db_service = DBService()
-
-    async def asyncTearDown(self):
-        await super().asyncTearDown()
-        await self.db_service.dispose()
-
     @pytest.mark.anyio
     async def test_insert_batch(self):
         data = await self.db_service.all(Hero)
