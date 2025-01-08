8c6136f09972cb90b109db77e71d006d700505b9
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-08 01:07:42 +0100

    :white_check_mark: Fixed warning default pytest_asyncio fixture scope

--- src/scripts/entrypoint.dev.sh
+++ src/scripts/entrypoint.dev.sh
@@ -9,6 +9,8 @@ testpaths =
     src/tests
 addopts = --rootdir ./src
 pythonpath = .
+asyncio_mode=auto
+asyncio_default_fixture_loop_scope=function
 """ > tox.ini
 
 fastapi dev main.py --app=app --host="${APP_HOST:-0.0.0.0}" --port="${APP_PORT:-80}"
