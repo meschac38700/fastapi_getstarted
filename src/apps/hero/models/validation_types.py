7f3888f6e279c6faff8dd854dc7f631dec9f0459
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-09 18:53:19 +0100

    :hammer: Add patch hero endpoint

--- src/apps/hero/models/validation_types.py
+++ src/apps/hero/models/validation_types.py
@@ -14,7 +14,21 @@ class HeroUpdate(HeroBase):
     class ConfigDict:
         orm_mode = False
 
+
+class HeroPatch(HeroBase):
+    name: str | None = None
+    secret_name: str | None = None
+    age: int | None = None
+
     def check_all_field_updated(self, old_data: dict[str, Any]):
-        current_attrs = self.model_dump().keys()
-        all_values_changed = all(old_data[k] != getattr(self, k) for k in current_attrs)
-        return current_attrs == old_data.keys() and all_values_changed
+        _old_data = old_data.copy()
+        _old_data.pop("id", None)
+        current_attrs = self.model_dump(exclude_unset=True).keys()
+        all_values_changed = all(
+            _old_data[k] != getattr(self, k) for k in current_attrs
+        )
+
+        return current_attrs == _old_data.keys() and all_values_changed
+
+    class ConfigDict:
+        orm_mode = False
--- src/apps/hero/routers.py
+++ src/apps/hero/routers.py
@@ -7,7 +7,7 @@ from fastapi.params import Query
 from services import DBServiceDep
 
 from .models import Hero
-from .models.validation_types import HeroCreate, HeroUpdate
+from .models.validation_types import HeroCreate, HeroPatch, HeroUpdate
 
 routers = APIRouter(tags=["heroes"], prefix="/heroes")
 
@@ -42,6 +42,23 @@ async def update_hero(pk: int, hero: HeroUpdate, db_service: DBServiceDep):
     stored_hero: Hero = await db_service.get(Hero, Hero.id == pk)
     if stored_hero is None:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Hero not found.")
+
+    stored_hero.update_from_dict(hero.model_dump(exclude_unset=True))
+    await db_service.insert(stored_hero)
+    return stored_hero
+
+
+@routers.patch(
+    "/{pk}", name="Update hero", response_model=Hero, status_code=HTTPStatus.OK
+)
+async def patch_hero(pk: int, hero: HeroPatch, db_service: DBServiceDep):
+    stored_hero: Hero = await db_service.get(Hero, Hero.id == pk)
+    if stored_hero is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Hero not found.")
+
+    if hero.check_all_field_updated(stored_hero.model_dump()):
+        detail = "Cannot use PATCH to update entire registry, use PUT instead."
+        raise HTTPException(status_code=HTTPStatus.METHOD_NOT_ALLOWED, detail=detail)
 
     stored_hero.update_from_dict(hero.model_dump(exclude_unset=True))
     await db_service.insert(stored_hero)
