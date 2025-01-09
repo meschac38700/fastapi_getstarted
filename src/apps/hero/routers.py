ee1253c6ec6221d6128e70b42f14b2732d526f2f
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-09 09:21:12 +0100

    :hammer: Add put endpoint

--- src/apps/hero/routers.py
+++ src/apps/hero/routers.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.params import Query
 
 from services import DBServiceDep
@@ -33,3 +33,16 @@ async def create_hero(hero: HeroCreate, db_service: DBServiceDep):
     _hero = Hero(**hero.model_dump())
     await db_service.insert(_hero)
     return _hero
+
+
+@routers.put(
+    "/{pk}", name="Update hero", response_model=Hero, status_code=HTTPStatus.OK
+)
+async def update_hero(pk: int, hero: HeroCreate, db_service: DBServiceDep):
+    stored_hero: Hero = await db_service.get(Hero, Hero.id == pk)
+    if stored_hero is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Hero not found.")
+
+    stored_hero.update_from_dict(hero.model_dump(exclude_unset=True))
+    await db_service.insert(stored_hero)
+    return stored_hero
