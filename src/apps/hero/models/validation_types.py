e1d0548c0a7aa2a4dedd5b452f09a34fd8fd0f02
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-09 18:34:22 +0100

    :hammer: Add HeroUpdate pydantic model

--- src/apps/hero/models/validation_types.py
+++ src/apps/hero/models/validation_types.py
@@ -1,6 +1,20 @@
+from typing import Any
+
 from ._base import HeroBase
 
 
 class HeroCreate(HeroBase):
     class ConfigDict:
         orm_mode = False
+
+
+class HeroUpdate(HeroBase):
+    age: int
+
+    class ConfigDict:
+        orm_mode = False
+
+    def check_all_field_updated(self, old_data: dict[str, Any]):
+        current_attrs = self.model_dump().keys()
+        all_values_changed = all(old_data[k] != getattr(self, k) for k in current_attrs)
+        return current_attrs == old_data.keys() and all_values_changed
