352909b1c657e0ee7224b9d10426b7bb67a3351e
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-12 23:31:42 +0100

    :truck: Rename handle to utils

--- src/core/db/__init__.py
+++ src/core/db/__init__.py
@@ -1,6 +1,6 @@
 from .base_models import SQLTable
-from .handle import create_db_and_tables, delete_db_and_tables
 from .session import SessionDep, get_session
+from .utils import create_db_and_tables, delete_db_and_tables
 
 __all__ = [
     "create_db_and_tables",
