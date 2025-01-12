b66ded9c063d968c955b9719cb63be8511dd4518
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-12 12:50:46 +0100

    :truck: Rename GET load data endpoint to POST /fixtures

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from core.db import DBServiceDep
 from core.db.fixtures import LoadFixtures
 from core.lifespan import setup, teardown
 from routers import register_app_routers
@@ -29,7 +28,7 @@ def secret_key(length: int = 65):
     return {"secret": secret}
 
 
-@app.get("/load_fake_data", name="Load some fake data.")
-async def load_fake_data(db_service: DBServiceDep):
+@app.post("/fixtures", name="Load app initial data.")
+async def load_fake_data():
     await LoadFixtures().load_fixtures()
     return {"Loaded": True}
