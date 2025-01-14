cb2c4714f844fcfffd0272431236cc15e344e812
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-14 14:27:51 +0100

    :rotating_light: Pre-commit configs

--- src/core/lifespan.py
+++ src/core/lifespan.py
@@ -1,11 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 
-async def setup(engine: AsyncEngine):
+async def setup(_: AsyncEngine):
     """Script to be run after fastapi setup."""
     # await create_db_and_tables(engine)
 
 
-async def teardown(engine: AsyncEngine):
+async def teardown(_: AsyncEngine):
     """Script to be run before fastapi shutdown."""
     # await delete_db_and_tables(engine)
