46454e6d80ce3aa0f6a91ddb29514cd1fa77463b
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-18 13:04:59 +0100

    :hammer: Add PUT/Patch endpoints

--- src/apps/user/routers.py
+++ src/apps/user/routers.py
@@ -1,8 +1,10 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from .models import User
+from .models.pydantic.create import UserCreate
+from .models.pydantic.patch import UserPatch
 
 routers = APIRouter(tags=["users"], prefix="/users")
 
@@ -15,3 +17,34 @@ async def get_users(offset: int = 0, limit=100):
 @routers.get("/{pk}", status_code=HTTPStatus.OK)
 async def get_user(pk: int):
     return await User.get(User.id == pk)
+
+
+@routers.put("/{pk}", status_code=HTTPStatus.OK)
+async def update_user(pk: int, user: UserCreate):
+    stored_user = await User.get(User.id == pk)
+    if stored_user is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found.")
+
+    user.check_all_required_fields_updated(stored_user.model_dump())
+    if not user.is_updated:
+        return stored_user
+
+    stored_user.update_from_dict(user.model_dump())
+    return await stored_user.save()
+
+
+@routers.patch("/{pk}", status_code=HTTPStatus.OK)
+async def patch_user(pk: int, user: UserPatch):
+    stored_user = await User.get(User.id == pk)
+    if stored_user is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found.")
+
+    if user.check_all_fields_updated(stored_user.model_dump()):
+        detail = "Cannot use PATCH to update entire object, use PUT instead."
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=detail)
+
+    if not user.is_updated:
+        return stored_user
+
+    stored_user.update_from_dict(user.model_dump(exclude_unset=True))
+    return await stored_user.save()
