d7916f8b516195d9854be839a86cf2cda8108845
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-01-18 22:50:25 +0100

    :white_check_mark: Fixed user tests: missing password field

--- src/apps/user/tests/test_user_crud_operations.py
+++ src/apps/user/tests/test_user_crud_operations.py
@@ -30,7 +30,9 @@ class TestUserCRUD(AsyncTestCase):
             "username": "jdoe",
             "first_name": "John",
             "last_name": "DOE",
+            "password": "jdoe",
         }
+        self.assertIsNone(await User.get(User.username == data["username"]))
         response = await self.client.post("/users/", json=data)
         self.assertEqual(HTTPStatus.CREATED, response.status_code)
 
@@ -40,12 +42,14 @@ class TestUserCRUD(AsyncTestCase):
         self.assertEqual(created_user.username, actual_user["username"])
         self.assertEqual(created_user.first_name, actual_user["first_name"])
         self.assertEqual(created_user.last_name, actual_user["last_name"])
+        self.assertTrue(created_user.check_password(data["password"]))
 
     async def test_patch_user(self):
         user = await User(
             username="jdoe",
             first_name="John",
             last_name="DOE",
+            password="jdoe",
             email="jdoe@example.test",
         ).save()
         data = {
@@ -65,12 +69,14 @@ class TestUserCRUD(AsyncTestCase):
             username="jdoe",
             first_name="John",
             last_name="DOE",
+            password="jdoe",
             email="jdoe@example.test",
         ).save()
         data = {
             "username": "doej",
             "first_name": "Jane",
             "last_name": "DOE",
+            "password": "doej",
             "age": date.today().year - 1970,
             "email": "john.doe@example.com",
             "address": "115 Place de Belledonne, Chamrousse",
@@ -88,6 +94,7 @@ class TestUserCRUD(AsyncTestCase):
             username="jdoe",
             first_name="John",
             last_name="DOE",
+            password="jdoe",
             email="jdoe@example.test",
         ).save()
         data = {
@@ -95,14 +102,16 @@ class TestUserCRUD(AsyncTestCase):
             "first_name": "jane",
             "last_name": "DOE",
             "email": "john.doe@example.com",
+            "password": "jdoe",
         }
         response = await self.client.patch(f"/users/{user.id}", json=data)
         self.assertEqual(HTTPStatus.OK, response.status_code)
 
-        patched_user = response.json()
+        user = await User.get(User.id == user.id)
 
-        self.assertEqual(data["username"], patched_user["username"])
-        self.assertEqual(data["email"], patched_user["email"])
+        self.assertEqual(data["username"], user.username)
+        self.assertEqual(data["email"], user.email)
+        self.assertTrue(user.check_password(data["password"]))
 
     async def test_delete_user(self):
         user_to_delete = await User.get(User.id == 1)
