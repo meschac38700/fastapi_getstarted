809d1dd34f4dc7b3dec0898764c3c6598ffe361e
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-02-01 17:16:32 +0100

    :loud_sound: Logs

--- src/apps/authorization/dependencies/user_permission.py
+++ src/apps/authorization/dependencies/user_permission.py
@@ -1,3 +1,4 @@
+import logging
 from http import HTTPStatus
 from typing import Any, Callable
 
@@ -8,20 +9,30 @@ from apps.authorization.models.permission import Permission
 
 Fn = Callable[..., Any]
 
+_logger = logging.getLogger(__name__)
+
 
 def user_permission_required(
     permissions: list[str],
     *,
     token: JWTToken,
     any_match: bool = False,
+    logger: logging.Logger | None = None,
 ) -> Fn:
+    log = logger or _logger
+
     async def dependency():
+        detail = "You do not have sufficient rights to this resource."
         permission_list = await Permission.filter(Permission.name.in_(permissions))
+
         user_has_permission = token.user.has_permissions(
             permission_list, any_match=any_match
         )
+
         if not user_has_permission:
-            detail = "You do not have sufficient rights to this resource."
+            any_all = f"{'any' if any_match else 'all'}"
+            msg = f"Permission denied: user{token.user.username} does not have {any_all} of the following rights: {permissions}."
+            log.debug(msg)
             raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail=detail)
 
         return token.user
