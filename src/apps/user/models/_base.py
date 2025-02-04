534689b6529d065b437001c6b12f3d3ec755134c
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-02-04 02:44:09 +0100

    :card_file_box: Add user role field

--- src/apps/user/models/_base.py
+++ src/apps/user/models/_base.py
@@ -1,5 +1,7 @@
+import sqlalchemy as sa
 from sqlmodel import Field
 
+from apps.user.utils.types import UserRole
 from core.db import SQLTable
 from core.db.models import TimestampedSQLBaseModel
 
@@ -12,6 +14,9 @@ class UserBase(SQLTable):
     email: str | None = Field(default=None, unique=True)
     address: str | None = None
     age: int | None = None
+    role: UserRole = Field(
+        sa_column=sa.Column(sa.Enum(UserRole), default=UserRole.active, index=True)
+    )
 
 
 class UserSQLBaseModel(UserBase, TimestampedSQLBaseModel):
