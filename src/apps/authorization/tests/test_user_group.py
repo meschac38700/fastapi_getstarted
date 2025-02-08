61dc217ef1d6c54cb197b321f8ad71bcf2b7df7f
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-02-08 23:13:44 +0100

    :test_tube: Test add/remove users to a certain group

---
+++ src/apps/authorization/tests/test_user_group.py
@@ -0,0 +1,80 @@
+from http import HTTPStatus
+
+from apps.authorization.models.group import Group
+from apps.user.models import User
+from apps.user.utils.types import UserRole
+from core.test.async_case import AsyncTestCase
+
+
+class TestUserGroup(AsyncTestCase):
+    fixtures = ["users"]
+
+    async def asyncSetUp(self):
+        await super().asyncSetUp()
+        self.admin = await User.get(User.role == UserRole.admin)
+        self.staff = await User.get(User.role == UserRole.staff)
+        self.active = await User.get(User.role == UserRole.active)
+
+    async def test_add_users_to_group(self):
+        group = await Group(
+            name="active_users",
+            target_table=User.table_name(),
+            display_name="Group of active users",
+        ).save()
+        self.assertEqual(group.users, [])
+
+        data = {
+            "users": [self.active.username, self.staff.username, self.admin.username]
+        }
+        await self.client.user_login(self.staff)
+        response = await self.client.post(
+            f"/authorizations/groups/{group.id}/users/add/", json=data
+        )
+        self.assertEqual(HTTPStatus.FORBIDDEN, response.status_code)
+
+        await self.client.user_login(self.admin)
+        response = await self.client.post(
+            f"/authorizations/groups/{group.id}/users/add/", json=data
+        )
+        print(response.json())
+        self.assertEqual(HTTPStatus.OK, response.status_code)
+        expected_response = [
+            self.admin.model_dump(mode="json"),
+            self.staff.model_dump(mode="json"),
+            self.active.model_dump(mode="json"),
+        ]
+        self.assertEqual(response.json(), expected_response)
+
+    async def test_remove_users_to_group(self):
+        group_admin = await Group(
+            name="administrators",
+            target_table="test",
+            display_name="Only for admin users.",
+        ).save()
+        self.assertEqual(group_admin.users, [])
+        users = await User.all()
+        await group_admin.extend_users(users)
+        self.assertEqual(group_admin.users, users)
+        data = {
+            "users": [
+                self.active.username,
+                self.staff.username,
+            ]
+        }
+        await self.client.user_login(self.staff)
+        response = await self.client.post(
+            f"/authorizations/groups/{group_admin.id}/users/remove/", json=data
+        )
+        self.assertEqual(HTTPStatus.FORBIDDEN, response.status_code)
+
+        await self.client.user_login(self.admin)
+        response = await self.client.post(
+            f"/authorizations/groups/{group_admin.id}/users/remove/", json=data
+        )
+        self.assertEqual(HTTPStatus.OK, response.status_code)
+        expected_response = [
+            self.admin.model_dump(mode="json"),
+        ]
+        self.assertEqual(response.json(), expected_response)
+        await group_admin.refresh()
+        self.assertEqual(group_admin.users, [self.admin])
