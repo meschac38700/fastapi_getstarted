55e501bfc63a3450af5fa11f82e6f28a811ccd46
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-02-08 23:15:32 +0100

    :hammer: Add users add/remove to a certain group endpoint

--- src/apps/authorization/routers/groups.py
+++ src/apps/authorization/routers/groups.py
@@ -6,6 +6,7 @@ from sqlmodel import and_
 from apps.authorization.models.group import Group
 from apps.authorization.models.pydantic.group import GroupCreate, GroupUpdate
 from apps.user.dependencies.roles import AdminAccess
+from apps.user.models.pydantic.create import UserList
 
 routers = APIRouter()
 
@@ -96,3 +97,33 @@ async def delete_group(pk: int):
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Group not found.")
 
     await group.delete()
+
+
+@routers.post(
+    "/{pk}/users/add/",
+    name="Add some users to a certain group",
+    dependencies=[Depends(AdminAccess())],
+)
+async def add_user_to_group(pk: int, users: UserList):
+    group = await Group.get(Group.id == pk)
+    if group is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Group not found.")
+
+    await group.extend_users(await users.to_object_list())
+
+    return group.users
+
+
+@routers.post(
+    "/{pk}/users/remove/",
+    name="Remove some user to a  certain group",
+    dependencies=[Depends(AdminAccess())],
+)
+async def remove_user_to_group(pk: int, users: UserList):
+    group = await Group.get(Group.id == pk)
+    if group is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Group not found.")
+
+    await group.remove_users(await users.to_object_list())
+
+    return group.users
--- src/apps/user/models/pydantic/create.py
+++ src/apps/user/models/pydantic/create.py
@@ -1,5 +1,16 @@
+from pydantic import BaseModel
+
+from apps.user.models import User
+
 from ._base import UserBaseModel
 
 
 class UserCreate(UserBaseModel):
     pass
+
+
+class UserList(BaseModel):
+    users: list[str]
+
+    async def to_object_list(self):
+        return await User.filter(User.username.in_(self.users))
