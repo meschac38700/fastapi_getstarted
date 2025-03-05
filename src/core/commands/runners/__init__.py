ec96f554e8a6613ca48f194704b84ba5ce395609
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-03-05 18:47:15 +0100

    :technologist: Add load fixtures command

--- src/core/commands/runners/__init__.py
+++ src/core/commands/runners/__init__.py
@@ -1,4 +1,5 @@
+from .fixtures import app as fixture_command
 from .server import app as dev_server_command
 from .tests import app as test_command
 
-__all__ = ["test_command", "dev_server_command"]
+__all__ = ["test_command", "dev_server_command", "fixture_command"]
