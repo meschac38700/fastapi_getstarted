8e872e384923a20bcfd69f20f53b36eafb8369c5
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-03-06 10:35:26 +0100

    :recycle: Rename command 'load-fixtures' to 'fixtures'

--- src/core/commands/runners/fixtures.py
+++ src/core/commands/runners/fixtures.py
@@ -13,7 +13,7 @@ app = typer.Typer(rich_markup_mode="rich")
 
 
 @app.command(help="Load project fixtures.")
-def load_fixtures(
+def fixtures(
     names: Annotated[
         list[str],
         typer.Option(
@@ -23,15 +23,15 @@ def load_fixtures(
 ):
     """Load project fixtures."""
 
-    fixtures = []
+    _fixtures = []
     if names:
-        fixtures = retrieve_app_fixtures(names)
+        _fixtures = retrieve_app_fixtures(names)
 
     fixture_loader = LoadFixtures(logger=_logger)
     try:
-        # TODO(Eliam): Typer does not support async command yet
+        # TODO(Eliam): Typer does not yet support async command
         #  Issue: https://github.com/fastapi/typer/issues/950
-        asyncio.run(fixture_loader.load_fixtures(fixtures))
+        asyncio.run(fixture_loader.load_fixtures(_fixtures))
     except IntegrityError:
         _logger.info(
             "It looks like loading fixtures might cause data integrity issues."
