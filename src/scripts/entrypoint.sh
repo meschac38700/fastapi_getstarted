716b91dbbae2b4d7eb60edf78559beb7b7e20d08
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-03-11 14:37:33 +0100

    :hammer: Retry applying migrations until success

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -3,6 +3,11 @@
 # Remove unused file copied for test environment
 rm -rf tox.ini
 
-alembic upgrade head
+# shellcheck disable=SC1009
+until alembic upgrade head 2> /dev/null;
+do
+  sleep 2
+  echo "Try apply migrations again.."
+done
 
 fastapi run main.py --app=app --host="${APP_HOST:-0.0.0.0}" --port="${APP_PORT:-80}"
