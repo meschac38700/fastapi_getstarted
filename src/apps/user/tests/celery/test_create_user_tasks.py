bd6b3657d51c7c95481e8ecc2f69348643496ca2
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-03-23 00:50:54 +0100

    :truck: Move test create user task into user app

---
+++ src/apps/user/tests/celery/test_create_user_tasks.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from apps.user.models import User
+from apps.user.tasks.user import create_user_task
+
+
+class TestCeleryTasks(TestCase):
+    def test_create_user_task(self):
+        user_data = {
+            "username": "pytest",
+            "first_name": "Celery",
+            "last_name": "FastApi",
+            "password": "test",
+        }
+        created_user = create_user_task.delay(**user_data).get(timeout=10)
+        assert isinstance(created_user, User)
+        assert created_user.first_name == user_data["first_name"]
+        assert created_user.last_name == user_data["last_name"]
+        assert created_user.username == user_data["username"]
+        assert created_user.check_password(user_data["password"])
