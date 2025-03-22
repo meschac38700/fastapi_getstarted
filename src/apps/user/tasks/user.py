8cffe61f9ae2b744a355009711d96eca63abddb2
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-03-23 00:49:39 +0100

    :truck: Move create user task into user app

--- src/apps/user/tasks/user.py
+++ src/apps/user/tasks/user.py
@@ -1,10 +1,9 @@
+import asyncio
+
+from apps.user.models import User
 from core.services.celery import app
 
 
 @app.task()
 def create_user_task(**user_data):
-    import asyncio
-
-    from apps.user.models import User
-
     return asyncio.run(User(**user_data).save())
--- src/core/tasks/__init__.py
+++ src/core/tasks/__init__.py
@@ -1,7 +1,5 @@
 from .basic import print_message
-from .user import create_user_task
 
 __all__ = [
-    "create_user_task",
     "print_message",
 ]
