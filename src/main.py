a2fd1633d4f367c0b1d21272bd901f26d20b705e
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-03-26 20:08:25 +0100

    :coffin: Remove unnecessary flake8 ignore instruction

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from core.lifespan import setup, teardown
 from core.monitoring.sentry import sentry_init
 from core.routers import register_default_endpoints
 from core.routers.register import AppRouter
-from core.services import celery  # noqa: F401
+from core.services import celery
 from settings import settings
 
 _engine = settings.get_engine()
