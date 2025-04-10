8567942c5af752675f0b94a675718ace341fea32
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-04-10 12:46:58 +0200

    :card_file_box: Add updated_at field

--- src/core/db/mixins/timestamped.py
+++ src/core/db/mixins/timestamped.py
@@ -1,7 +1,7 @@
 import datetime
 
-from sqlalchemy import text
-from sqlmodel import Field
+from sqlalchemy import func, text
+from sqlmodel import DateTime, Field
 
 
 class TimestampedModelMixin:
@@ -9,4 +9,9 @@ class TimestampedModelMixin:
         sa_column_kwargs={
             "server_default": text("CURRENT_TIMESTAMP"),
         }
+    )
+    updated_at: datetime.datetime | None = Field(
+        default=None,
+        sa_type=DateTime,
+        sa_column_kwargs={"onupdate": func.now(), "nullable": True},
     )
