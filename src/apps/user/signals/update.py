82972e2efb219573f3074914e4c64d45973bacbd
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-04-30 16:24:46 +0200

    :technologist: Typing function arguments

--- src/apps/user/signals/update.py
+++ src/apps/user/signals/update.py
@@ -1,3 +1,6 @@
+from sqlalchemy import Connection
+from sqlalchemy.orm import Mapper
+
 from apps.user.models import User
 from core.db.signals.managers import signal_manager
 from core.monitoring.logger import get_logger
@@ -6,12 +9,16 @@ _logger = get_logger(__name__)
 
 
 @signal_manager.before_update(User)
-def before_update(*args, **kwargs):
+def before_update(mapper: Mapper[User], connection: Connection, user: User):
     # TODO(Eliam) wip
-    _logger.info(f"-----------> Signal User before update: {args}, {kwargs}")
+    _logger.info(
+        f"-----------> Signal User before update: {mapper}, {connection}, {user}."
+    )
 
 
 @signal_manager.after_update(User)
-def after_update(*args, **kwargs):
+def after_update(mapper: Mapper[User], connection: Connection, user: User):
     # TODO(Eliam) wip
-    _logger.info(f"-----------> Signal User after update: {args}, {kwargs}")
+    _logger.info(
+        f"-----------> Signal User after update: {mapper}, {connection}, {user}."
+    )
