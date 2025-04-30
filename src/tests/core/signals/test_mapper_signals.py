7943ee49fc4017460c024bc35ee44bbce6e9aae1
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-04-30 18:55:15 +0200

    :white_check_mark: Test mapper signal: before_update

---
+++ src/tests/core/signals/test_mapper_signals.py
@@ -0,0 +1,25 @@
+from sqlalchemy.engine.base import Connection
+from sqlalchemy.orm import Mapper
+
+from apps.user.models import User
+from core.db.signals.managers import signal_manager
+from core.testing.async_case import AsyncTestCase
+
+
+class TestMapperSignal(AsyncTestCase):
+    fixtures = ["users"]
+
+    async def asyncSetUp(self):
+        await super().asyncSetUp()
+        self.user = await User.get(id=1)
+
+    async def test_before_update_signal(self):
+        def double_user_age(_: Mapper[User], __: Connection, user: User):
+            user.age *= 2
+
+        new_age = 10
+        signal_manager.before_update(User)(double_user_age)
+        self.user.age = new_age
+        await self.user.save()
+
+        self.assertEqual(new_age * 2, self.user.age)
