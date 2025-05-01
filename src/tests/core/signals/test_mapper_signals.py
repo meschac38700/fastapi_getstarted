876ead5efc230fa6a8de3ef7aaf9a0581eafb4dd
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-05-01 23:23:20 +0200

    :white_check_mark: Add test signal before_insert

--- src/tests/core/signals/test_mapper_signals.py
+++ src/tests/core/signals/test_mapper_signals.py
@@ -3,6 +3,7 @@ import asyncio
 from sqlalchemy.engine.base import Connection
 from sqlalchemy.orm import Mapper
 
+from apps.authorization.models import Permission
 from apps.user.models import User
 from core.db.signals.managers import signal_manager
 from core.testing.async_case import AsyncTestCase
@@ -45,3 +46,19 @@ class TestMapperSignal(AsyncTestCase):
 
         # Unregister to avoid distorting next tests
         signal_manager.unregister("after_update", set_second_user_age, target=User)
+
+    async def test_before_insert_signal(self):
+        permissions = await Permission.filter(target_table=User.table_name())
+
+        def set_permissions(_: Mapper[User], __: Connection, user: User):
+            user.permissions = permissions
+
+        signal_manager.before_insert(User)(set_permissions)
+        new_user = await User(
+            username="foo", first_name="bar", last_name="DOE", password="pytest"
+        ).save()
+
+        self.assertEqual(new_user.permissions, permissions)
+
+        # Unregister to avoid distorting next tests
+        signal_manager.unregister("before_insert", set_permissions, target=User)
