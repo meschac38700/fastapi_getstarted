9933a596b1ac338389ea949c90fe5bc0748ecf8f
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-05-01 11:49:56 +0200

    :white_check_mark: Add test register/unregister a signal

---
+++ src/tests/core/signals/test_register_signal.py
@@ -0,0 +1,42 @@
+from typing import Callable
+
+import pytest
+from sqlalchemy import event as sa_event
+from typing_extensions import Any, ParamSpec
+
+from apps.user.models import User
+from core.db.signals.managers import signal_manager
+
+P = ParamSpec("P")
+Fn = Callable[P, Any]
+
+
+def foo(*_, **__):
+    pass
+
+
+@pytest.mark.parametrize(
+    "target,event_name,callback",
+    (
+        (User, "before_update", foo),
+        (User, "before_insert", foo),
+        (User, "after_delete", foo),
+    ),
+)
+def test_register_signal(target: Any, event_name: str, callback: Fn):
+    signal_manager.register(event_name, callback, target=target)
+    assert sa_event.contains(target, event_name, callback)
+    assert len(signal_manager.event_handlers) >= 1
+
+
+@pytest.mark.parametrize(
+    "target,event_name,callback",
+    (
+        (User, "before_update", foo),
+        (User, "before_insert", foo),
+        (User, "after_delete", foo),
+    ),
+)
+def test_unregister_signal(target: Any, event_name: str, callback: Fn):
+    signal_manager.unregister(event_name, callback, target=target)
+    assert sa_event.contains(target, event_name, callback) is False
