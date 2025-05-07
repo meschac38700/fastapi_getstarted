3b2c3c644e7488ef7f5d184f2ca6b98e19d72f5b
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-05-08 00:05:28 +0200

    :white_check_mark: Reviewed test before/after create model table

--- src/tests/core/signals/test_schema_signals.py
+++ src/tests/core/signals/test_schema_signals.py
@@ -1,28 +1,53 @@
-from sqlalchemy import Connection, Table
+from collections.abc import Callable
+from typing import Any
 
-from apps.user.models import User
+from sqlalchemy import Connection, Table
+from sqlmodel import Field
+
+from core.db import SQLTable
 from core.db.signals.managers import signal_manager
 from core.testing.async_case import AsyncTestCase
+from settings import settings
+
+Fn = Callable[..., Any]
 
 
 class TestSchemaSignals(AsyncTestCase):
-    def register_listeners(self):
-        @signal_manager.before_create(User.table())
-        def before_create_user_table(target: Table, _: Connection, **__):
-            self.before_create_user_table_called = target.name == User.table_name()
+    async def _db_exec(self, callback: Fn, **kwargs):
+        engine = settings.get_engine()
+        async with engine.connect() as conn:
+            await conn.run_sync(lambda sync_conn: callback(sync_conn, **kwargs))
 
-        @signal_manager.after_create(User.table())
-        def after_create_user_table(target: Table, _: Connection, **__):
-            self.after_create_user_table_called = target.name == User.table_name()
+    async def test_before_create_signal(self):
+        class BeforeCreateModel(SQLTable, table=True):
+            id: int = Field(primary_key=True, allow_mutation=False)
+            name: str
 
-    async def asyncSetUp(self):
-        self.before_create_user_table_called = False
-        self.after_create_user_table_called = False
-        self.register_listeners()
-        await super().asyncSetUp()
+        table = BeforeCreateModel.table()
+        before_create_table_called = False
 
-    def test_before_create_signal(self):
-        self.assertTrue(self.before_create_user_table_called)
+        @signal_manager.before_create(table)
+        def before_create_table(target: Table, _: Connection, **__):
+            """Listener for before create model table."""
+            nonlocal before_create_table_called
+            before_create_table_called = target.name == BeforeCreateModel.table_name()
 
-    def test_after_create_signal(self):
-        self.assertTrue(self.after_create_user_table_called)
+        await self._db_exec(table.create)
+        self.assertTrue(before_create_table_called)
+
+    async def test_after_create_signal(self):
+        class AfterCreateModel(SQLTable, table=True):
+            id: int = Field(primary_key=True, allow_mutation=False)
+            name: str
+
+        table = AfterCreateModel.table()
+
+        after_create_table_called = False
+
+        @signal_manager.after_create(table)
+        def after_create_table(target: Table, _: Connection, **__):
+            nonlocal after_create_table_called
+            after_create_table_called = target.name == AfterCreateModel.table_name()
+
+        await self._db_exec(table.create)
+        self.assertTrue(after_create_table_called)
