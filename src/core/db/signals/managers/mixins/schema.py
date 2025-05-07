9d4270633b35ba5ddcb333745ce8830b6e19552d
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-05-07 20:50:40 +0200

    :recycle: Code refactoring, add more docs

--- src/core/db/signals/managers/mixins/schema.py
+++ src/core/db/signals/managers/mixins/schema.py
@@ -1,6 +1,6 @@
 from typing import Callable
 
-from sqlmodel import SQLModel
+from sqlalchemy import Table
 from typing_extensions import Any, ParamSpec
 
 from core.db.signals.utils.types import EventCategory
@@ -14,9 +14,21 @@ class SchemaEventMixin:
     SchemaEventTarget subclasses, including MetaData, Table, Column, etc.
 
     Docs: https://docs.sqlalchemy.org/en/20/core/events.html#schema-events
+
+    Example:
+    >>> from sqlalchemy import Connection, Table
+    >>>
+    >>> from core.db.signals.managers import signal_manager
+    >>> from apps.user.models import User
+    >>>
+    >>>
+    >>> @signal_manager.before_create(User.table())
+    >>> def before_create_user_table(target: Table, connection: Connection, **kwargs):
+    >>>     pass
+    >>>
     """
 
-    def before_create(self, model: SQLModel):
+    def before_create(self, model: Table):
         """Handle event before CREATE sql command is emitted to the database."""
 
         def wrapper(callback: Fn):
@@ -26,7 +38,7 @@ class SchemaEventMixin:
 
         return wrapper
 
-    def before_drop(self, model: SQLModel):
+    def before_drop(self, model: Table):
         """Handle event before DROP sql command is emitted to the database."""
 
         def wrapper(callback: Fn):
@@ -36,7 +48,7 @@ class SchemaEventMixin:
 
         return wrapper
 
-    def after_create(self, model: SQLModel):
+    def after_create(self, model: Table):
         """Handle event after CREATE sql command is emitted to the database."""
 
         def wrapper(callback: Fn):
@@ -46,7 +58,7 @@ class SchemaEventMixin:
 
         return wrapper
 
-    def after_drop(self, model: SQLModel):
+    def after_drop(self, model: Table):
         """Handle event after DROP sql command is emitted to the database."""
 
         def wrapper(callback: Fn):
