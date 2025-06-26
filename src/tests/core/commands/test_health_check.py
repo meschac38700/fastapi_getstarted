933a987098e77de5e9307075aaac97ab8feaa452
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-06-26 22:03:58 +0200

    :recycle: Update test name

--- src/tests/core/commands/test_health_check.py
+++ src/tests/core/commands/test_health_check.py
@@ -8,7 +8,7 @@ runner = CliRunner()
 
 
 @pytest.mark.asyncio
-def test_command_heath_check_ok(httpx_mock: HTTPXMock):
+def test_command_heath_check_success(httpx_mock: HTTPXMock):
     httpx_mock.add_response(200, json={"status": "ok"})
 
     result = runner.invoke(app, ["healthcheck"])
