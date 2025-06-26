f4491533238715fae07804914043ae813ce2171e
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-06-26 21:44:39 +0200

    :white_check_mark: Fixed test health check command

--- src/tests/core/commands/test_health_check.py
+++ src/tests/core/commands/test_health_check.py
@@ -9,7 +9,7 @@ runner = CliRunner()
 
 @pytest.mark.asyncio
 def test_command_heath_check_ok(httpx_mock: HTTPXMock):
-    httpx_mock.add_response(200)
+    httpx_mock.add_response(200, json={"status": "ok"})
 
     result = runner.invoke(app, ["healthcheck"])
     assert result.exit_code == 0
