8e8702b60cb4ba1440ec8a3e005ea69ef8bdb9c1
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-07-11 12:21:22 +0200

    :hammer: Distinct functions: render & render_string

--- src/core/templating/utils.py
+++ src/core/templating/utils.py
@@ -1,8 +1,20 @@
+from http import HTTPStatus
 from typing import Any
+
+from starlette.responses import HTMLResponse
 
 from core.templating.loaders import app_template_loader
 
 
-def render(template_name: str, context: dict[str, Any] = None) -> str:
-    """Render application template with given context."""
+def render_string(template_name: str, context: dict[str, Any] = None) -> str:
+    """Render application template with given context in string format."""
     return app_template_loader.get_template(template_name).render(**(context or {}))
+
+
+def render(
+    template_name: str,
+    context: dict[str, Any] = None,
+    status_code: HTTPStatus = HTTPStatus.OK,
+) -> HTMLResponse:
+    """Return HTMLResponse of the template with given context."""
+    return HTMLResponse(render_string(template_name, context), status_code)
--- src/tests/core/templating/test_utils.py
+++ src/tests/core/templating/test_utils.py
@@ -1,11 +1,12 @@
 import shutil
+from http import HTTPStatus
 from pathlib import Path
 from unittest.mock import patch
 
 import pytest
 
+from core.templating import utils as http_utils
 from core.templating.loaders import app_template_loader
-from core.templating.utils import render
 
 
 @pytest.fixture
@@ -20,7 +21,7 @@ def template_dir(app_dir: Path) -> Path:
     return template_dir
 
 
-def test_get_render_template(app_dir: Path, template_dir: Path):
+def test_get_render_template_as_string(app_dir: Path, template_dir: Path):
     with patch("core.templating.loaders.apps.file_services") as mock_file_services:
         mock_file_services.get_application_paths.return_value = [app_dir]
         index_file = template_dir / "index.html"
@@ -28,12 +29,32 @@ def test_get_render_template(app_dir: Path, template_dir: Path):
         expected_text = "<h1>render template: {{template_name}}</h1>"
         index_file.write_text(expected_text)
 
-        template_content = render(
+        template_content = http_utils.render_string(
             index_file.name, context={"template_name": index_file.name}
         )
 
         assert template_content == expected_text.replace(
             "{{template_name}}", index_file.name
         )
+        # clear test data
+        shutil.rmtree(str(template_dir), ignore_errors=True)
+
+
+def test_get_render_template_as_html_response(app_dir: Path, template_dir: Path):
+    with patch("core.templating.loaders.apps.file_services") as mock_file_services:
+        mock_file_services.get_application_paths.return_value = [app_dir]
+        index_file = template_dir / "index.html"
+
+        expected_text = "<h1>render template: {{template_name}}</h1>"
+        index_file.write_text(expected_text)
+
+        html_response = http_utils.render(
+            index_file.name, context={"template_name": index_file.name}
+        )
+
+        assert HTTPStatus.OK == html_response.status_code
+        assert expected_text.replace(
+            "{{template_name}}", index_file.name
+        ) == html_response.body.decode("utf-8")
         # clear test data
         shutil.rmtree(str(template_dir), ignore_errors=True)
