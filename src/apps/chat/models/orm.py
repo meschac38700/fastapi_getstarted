e14cb681e3328a55a8a591a7278143310801687d
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-07-19 16:36:30 +0200

    :card_file_box: Add chat models: ChatRoom & ChatMessage

---
+++ src/apps/chat/models/orm.py
@@ -0,0 +1,37 @@
+from sqlmodel import Field, Relationship
+
+from apps.chat.models.base import ChatMessageBaseModel, ChatRoomBaseModel
+from apps.user.models import User
+from core.db.mixins import BaseTable
+
+
+class ChatRoom(ChatRoomBaseModel, BaseTable, table=True):
+    owner_id: int | None = Field(
+        foreign_key="users.id", default=None, ondelete="CASCADE"
+    )
+    owner: User = Relationship(sa_relationship_kwargs={"lazy": "joined"})
+    messages: list["ChatMessage"] = Relationship(
+        sa_relationship_kwargs={"lazy": "left"}, back_populates="room"
+    )
+    members: list[User] = Relationship(sa_relationship_kwargs={"lazy": "left"})
+
+    async def subscribe(self, member: User):
+        self.members.append(member)
+        return await self.save()
+
+    async def unsubscribe(self, member: User):
+        self.members.remove(member)
+        return await self.save()
+
+
+class ChatMessage(ChatMessageBaseModel, BaseTable, table=True):
+    author_id: int | None = Field(
+        foreign_key="users.id", default=None, ondelete="SET NULL"
+    )
+    author: User = Relationship(sa_relationship_kwargs={"lazy": "joined"})
+    room_id: int | None = Field(
+        foreign_key="chatroom.id", default=None, ondelete="CASCADE"
+    )
+    room: ChatRoom = Relationship(
+        sa_relationship_kwargs={"lazy": "joined"}, back_populates="messages"
+    )
