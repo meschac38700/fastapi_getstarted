eb5dfd7eefd8ebdb29cb664a1ab9e4dc047466f6
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-07-21 01:05:35 +0200

    :white_check_mark: Test staticfiles are loaded correctly inside a template

---
+++ src/tests/core/templating/test_staticfiles.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from fastapi import status
+
+
+async def test_load_static_file_in_template(
+    static_root: Path, template_dir, setup_test_routes, client
+):
+    css_filename = "styles.css"
+    css_file = static_root / css_filename
+    css_file.write_text("html, body {color: red;}")
+
+    css_html_link = (
+        "<link href=\"{{ url_for('static', path='/"
+        + css_filename
+        + '\') }}" rel="stylesheet" />'
+    )
+
+    index_file = template_dir / "index.html"
+    index_file.write_text(css_html_link)
+
+    response = await client.get(setup_test_routes)
+
+    assert response.status_code == status.HTTP_200_OK
+    expected_html_link = (
+        '<link href="https://test/static/styles.css" rel="stylesheet" />'
+    )
+    assert expected_html_link == response.content.decode("utf-8")
