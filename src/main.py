0e3586bc5c41313c90a4020649834968f985012b
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-07-20 10:52:55 +0200

    :lipstick: Setup staticfiles

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
 from core.db.dependencies.session import get_engine
 from core.db.signals.main import setup_signals
@@ -11,6 +12,8 @@ from core.routers import register_default_endpoints
 from core.routers.register import AppRouter
 from core.security.csrf import csrf_exception_handler
 from core.services import celery
+from core.services.files import apps as file_apps
+from settings import settings
 
 _engine = get_engine()
 
@@ -26,6 +29,14 @@ async def lifespan(_: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.celery = celery
+static_packages = file_apps.static_packages()
+app.mount(
+    settings.STATIC_URL,
+    StaticFiles(
+        check_dir=False, packages=static_packages, directory=settings.STATIC_ROOT
+    ),
+    name="static",
+)
 
 csrf_exception_handler(app)
 cors_middleware(app)
