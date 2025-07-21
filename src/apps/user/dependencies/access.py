5052c8831d51d7a81270c6aa1cd2eb814446f7d4
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-07-22 00:25:09 +0200

    :recycle: Refactoring user access dependency code

---
+++ src/apps/user/dependencies/access.py
@@ -0,0 +1,24 @@
+from abc import abstractmethod
+
+from fastapi import Depends
+
+from apps.authentication.dependencies.oauth2 import current_user
+from apps.user.models import User
+from core.routers.dependencies import AccessDependency
+
+
+class UserAccess[T = User](AccessDependency[User]):
+    user: User
+
+    @abstractmethod
+    def test_access(self) -> bool:
+        pass
+
+    async def __call__(self, user: User = Depends(current_user())) -> T:
+        self.user = user
+        test_pass = self.test_access()
+
+        if not test_pass:
+            self.raise_access_denied()
+
+        return user
--- src/apps/user/dependencies/roles.py
+++ src/apps/user/dependencies/roles.py
@@ -1,37 +1,16 @@
-from abc import ABC, abstractmethod
-from http import HTTPStatus
-
-from fastapi import Depends, HTTPException
-
-from apps.authentication.dependencies.oauth2 import current_user
-from apps.user.models import User
+from apps.user.dependencies.access import UserAccess
 
 
-class RoleAccess(ABC):
-    @abstractmethod
-    def _has_access(self, user: User) -> bool:
-        pass
-
-    async def __call__(self, user: User = Depends(current_user())):
-        allowed = self._has_access(user)
-
-        if not allowed:
-            detail = "Insufficient rights to carry out this action"
-            raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail=detail)
-
-        return allowed
+class AdminAccess(UserAccess):
+    def test_access(self) -> bool:
+        return self.user.is_admin
 
 
-class AdminAccess(RoleAccess):
-    def _has_access(self, user: User) -> bool:
-        return user.is_admin
+class StaffAccess(UserAccess):
+    def test_access(self) -> bool:
+        return self.user.is_admin
 
 
-class StaffAccess(RoleAccess):
-    def _has_access(self, user: User) -> bool:
-        return user.is_admin
-
-
-class ActiveAccess(RoleAccess):
-    def _has_access(self, user: User) -> bool:
-        return user.is_admin
+class ActiveAccess(UserAccess):
+    def test_access(self) -> bool:
+        return self.user.is_admin
