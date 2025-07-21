a051e44f8a84bbebce1d62c70a30a39968fa9a39
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-07-21 22:15:15 +0200

    :test_tube: Test get all rooms: paginate response expected

--- src/apps/chat/tests/test_chat_room_endpoints.py
+++ src/apps/chat/tests/test_chat_room_endpoints.py
@@ -47,7 +47,13 @@ async def test_get_chat_rooms(
     await client.force_login(admin)
     response = await client.get(app.url_path_for("room-all"))
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == [room.model_dump(mode="json")]
+    assert response.json() == {
+        "items": [room.model_dump(mode="json")],
+        "total": 1,
+        "page": 1,
+        "size": 50,
+        "pages": 1,
+    }
 
 
 async def test_get_chat_room(
