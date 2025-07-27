28bc3ffb9d2962e66f89305188210c9367ba0774
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-07-27 12:31:19 +0200

    :card_file_box: Add Unique constraint

--- src/apps/chat/models/relation_links.py
+++ src/apps/chat/models/relation_links.py
@@ -1,9 +1,11 @@
+from sqlalchemy import UniqueConstraint
 from sqlmodel import Field
 
 from core.db import SQLTable
 
 
 class ChatRoomUserLink(SQLTable, table=True):
+    __table_args__ = (UniqueConstraint("room_id", "user_id", name="chatroom_user_id"),)
     room_id: int | None = Field(
         default=None, foreign_key="chatroom.id", primary_key=True, ondelete="CASCADE"
     )
