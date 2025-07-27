475a64efa602f0e30785e31e49098d873e923537
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-07-27 13:59:02 +0200

    :recycle: Code Refactoring: dedicate file for message routes

--- src/apps/chat/routers/__init__.py
+++ src/apps/chat/routers/__init__.py
@@ -2,7 +2,11 @@ from .chat import routers
 from .message import routers as message_routers
 from .room import routers as room_routers
 
+room_routers.include_router(
+    message_routers, prefix="/{room_id}/messages", tags=["chat", "message"]
+)
+
 routers.include_router(room_routers, prefix="/rooms", tags=["chat", "room"])
-routers.include_router(message_routers, prefix="/messages", tags=["chat", "message"])
+
 
 __all__ = ["routers"]
--- src/apps/chat/routers/room.py
+++ src/apps/chat/routers/room.py
@@ -8,14 +8,12 @@ from sqlalchemy.exc import IntegrityError
 
 from apps.authentication.dependencies.oauth2 import current_user
 from apps.chat.dependencies.access import (
-    ChatMessageDeleteAccess,
     ChatRoomAccess,
     ChatRoomDeleteAccess,
     ChatRoomEditAccess,
 )
 from apps.chat.dependencies.db import RoomDepends
-from apps.chat.models import ChatMessage, ChatRoom
-from apps.chat.models.schemas.message import ChatMessageCreate
+from apps.chat.models import ChatRoom
 from apps.chat.models.schemas.room import ChatRoomCreate, ChatRoomUpdate
 from apps.user.dependencies.roles import AdminAccess
 from apps.user.models import User
@@ -48,40 +46,6 @@ async def get_rooms(db: SessionDep) -> Page[ChatRoom]:
 @routers.get("/{room_id}/", name="room-get")
 def get_room(room: Annotated[ChatRoom, Depends(ChatRoomAccess())]) -> ChatRoom:
     return room
-
-
-@routers.get("/{room_id}/messages/", name="room-messages")
-async def get_room_messages(
-    db: SessionDep, room: Annotated[ChatRoom, Depends(ChatRoomAccess())]
-) -> Page[ChatMessage]:
-    query = (
-        select(ChatMessage)
-        .where(ChatMessage.room_id == room.id)
-        .order_by(ChatMessage.created_at)
-    )
-    return await apaginate(db, query)
-
-
-@routers.post("/{room_id}/messages/", name="room-message-add")
-async def add_room_message(
-    message: ChatMessageCreate,
-    room: Annotated[ChatRoom, Depends(ChatRoomAccess())],
-    auth_user: User = Depends(current_user()),
-):
-    return await ChatMessage(
-        **message.model_dump(), author_id=auth_user.id, room_id=room.id
-    ).save()
-
-
-@routers.delete(
-    "/{room_id}/messages/{message_id}",
-    name="room-message-delete",
-    status_code=status.HTTP_204_NO_CONTENT,
-)
-async def delete_room_message(
-    message: Annotated[ChatMessage, Depends(ChatMessageDeleteAccess())],
-):
-    return await message.delete()
 
 
 @routers.patch(
