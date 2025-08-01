c1a91abb4f1e1dd3ede345ba42fb8e2964cf5d58
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-08-01 20:47:58 +0200

    :adhesive_bandage: Fixed: use token auth to display swagger authorize button

--- src/apps/user/dependencies/access.py
+++ src/apps/user/dependencies/access.py
@@ -2,10 +2,9 @@ from abc import abstractmethod
 
 from fastapi import Depends
 
-from apps.authentication.dependencies.oauth2 import (
-    current_session_user,
-    current_user,
-)
+from apps.authentication.dependencies import oauth2_scheme
+from apps.authentication.dependencies.oauth2 import current_session_user
+from apps.authentication.models import JWTToken
 from apps.user.dependencies.exceptions import AccessDeniedError
 from apps.user.models import User
 from core.routers.dependencies import AccessDependency
@@ -18,14 +17,14 @@ class UserAccess[T = User](AccessDependency[User]):
     def test_access(self) -> bool:
         pass
 
-    async def __call__(self, user: User = Depends(current_user)) -> T:
-        self.user = user
+    async def __call__(self, token: JWTToken = Depends(oauth2_scheme())) -> T:
+        self.user = token.user
         test_pass = self.test_access()
 
         if not test_pass:
             self.raise_access_denied()
 
-        return user
+        return self.user
 
 
 class AnonymousUserAccess(UserAccess[User]):
