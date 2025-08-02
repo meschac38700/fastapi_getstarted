49d1d8b8f3a14d9b62fe9ada2951a723611c3597
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-08-02 20:55:17 +0200

    :lipstick: Fixed handle ws messages

---
+++ src/apps/chat/statics/chat/js/new-messages.js
@@ -0,0 +1,53 @@
+(() => {
+    const wsURL = document.currentScript.dataset.wsUrl
+
+    function createMessage(message){
+        let msg = ""
+        if (message.author_id !== window.lastMessageAuthor)
+            msg = `
+                <div class="message">
+                    <div class="photo" style="background-image: url(https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80);">
+                        <div class="online"></div>
+                    </div>
+                    <p class="text">${ message.content }</p>
+                </div>
+            `
+        else
+            msg = `
+                <div class="message text-only">
+                    <p class="text">${ message.content }</p>
+                </div>
+            `
+        window.lastMessageAuthor = message.author_id
+        return msg
+    }
+
+    window.createMessage = createMessage
+
+    function initWSConnection(roomID){
+        const WSUrl = wsURL.replace("/-1/", `/${roomID}/`)
+        const ws = new WebSocket(WSUrl)
+
+        ws.onmessage = function (event) {
+            const messages = document.getElementById("room-conversation")
+            const message = window.createMessage(JSON.parse(JSON.parse(event.data)))
+            messages.innerHTML += message
+        }
+
+        const form = document.getElementById("ws-form")
+        form.addEventListener("submit", function(e){
+            e.preventDefault()
+            e.stopPropagation()
+
+            const messageInput = this.querySelector("#message")
+            const newMessage = messageInput.value.trim()
+            if(newMessage)
+                ws.send(newMessage)
+
+            messageInput.value = ""
+        })
+    }
+    window.initWSConnection = initWSConnection
+
+
+})()
