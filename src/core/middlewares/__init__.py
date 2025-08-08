2c65622eaa7a01cbe204fb01186a189db069ed36
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-08-08 12:54:59 +0200

    :hammer: Add middleware ProxyHeadersMiddleware

--- src/core/middlewares/__init__.py
+++ src/core/middlewares/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 
 from .auth import auth_related_middlewares
 from .cors import cors_middleware
@@ -9,6 +10,7 @@ def register_middlewares(app: FastAPI):
     cors_middleware(app)
     auth_related_middlewares(app)
     session_middleware(app)
+    app.add_middleware(ProxyHeadersMiddleware, trusted_hosts=["*"])
 
 
 __all__ = ["register_middlewares"]
