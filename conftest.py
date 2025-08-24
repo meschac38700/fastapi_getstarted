3a3c7b359ac4c3ecd04420028261a111005777b8
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-08-24 13:43:03 +0200

    :bug: Fixed db name length

--- conftest.py
+++ conftest.py
@@ -69,7 +69,7 @@ async def client(app, app_url):
 def db_name(request, worker_id):
     method_name = request.node.name
     _uuid = uuid.uuid4().hex[:6]
-    db_name = f"{_uuid}_{method_name[:50]}_{worker_id}"
+    db_name = f"{_uuid}_{method_name[:40]}_{worker_id}"
     return db_name
 
 
